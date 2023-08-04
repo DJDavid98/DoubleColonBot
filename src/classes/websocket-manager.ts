a53d4bf2ef9aba612d8ef626f28cdbd71fe78c4c
DJDavid98 <djdavid98@protonmail.com>
2023-08-04 02:21:22 +0200

    Allow localhost and overlay through CORS for socket server

--- src/classes/websocket-manager.ts
+++ src/classes/websocket-manager.ts
@@ -7,7 +7,10 @@ export class WebsocketManager {
   private io: Server<ClientToServerEvents, ServerToClientEvents>;
 
   constructor(server: http.Server, private logger: Logger) {
-    this.io = new Server(server, { serveClient: false }); // Create socket.io instance
+    this.io = new Server(server, {
+      serveClient: false,
+      cors: { origin: /^(https?:\/\/)?(localhost(:\d+)?|overlay.djdavid98.art)$/ },
+    }); // Create socket.io instance
     this.io.on('connection', this.onConnection.bind(this));
   }
 
